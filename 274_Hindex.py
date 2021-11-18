826087b0eafbc6c63392869f985f7eed186e7c97
dauhui <rexyhyang@gmail.com>
2021-11-18 14:57:42 +0800

    runtime modified

--- 274_Hindex.py
+++ 274_Hindex.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def hIndex(self, citations):
-        # n = len(citations)
-        # for h in range(n):
-        #     cnt = len([item for item in citations if item >= 0])
-        #     if (n - h) > cnt:
-        #         return  h
-        #     citations[:] = [item - 1 for item in citations]
         ans = 0
         for h in range(max(citations) + 1):
             cnt = len([item for item in citations if item >= h])
