f06129c594143383f98aa870d8528f5e48d19144
dauhui <rexyhyang@gmail.com>
2021-11-24 15:59:17 +0800

    first upload

--- 189_rotatearray.py
+++ 189_rotatearray.py
@@ -5,4 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
+        # reference from discussion
+        k = k%len(nums)
+
+        for i in range(k):
+            p = nums.pop()
+            nums.insert(0, p)
+
+
+
+A = [1,2,3,4,5,6,7]
+ans = Solution.rotate(Solution, A, 3)
+print(ans)
