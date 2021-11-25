9832d2d619b52475bff605af41a486aec394104a
dauhui <rexyhyang@gmail.com>
2021-11-25 16:44:33 +0800

    first upload

--- 566_reshape.py
+++ 566_reshape.py
@@ -6,15 +6,17 @@ class Solution(object):
         :type c: int
         :rtype: List[List[int]]
         """
+        if r*c != len(mat)*len(mat[0]):
+            return  mat
         temp = []
         output = []
         for i in range(len(mat)):
             temp += mat[i]
         for i in range(r):
-            output.insert(i, temp[0:4])
-            del temp[0:4]
+            output.insert(i, temp[0:c])
+            del temp[0:c]
         return  output
 
-A = [[1,2],[3,4]]
+A = [[1,2]]
 ans = Solution.matrixReshape(Solution, A, 1, 4)
 print(ans)
